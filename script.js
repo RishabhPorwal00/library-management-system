c75152036de666ace25aa47a1e3ff0943f7f4cdb
Rishabh Porwal <rishabhporwal06@gmail.com>
2026-09-11 01:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -227,7 +227,9 @@ function displayBooks() {
             book.issued
                 ? "Issued"
                 : "Available";
-
+let statusClass = book.issued
+    ? "status-issued"
+    : "status-available";
 
         let actionButton =
             book.issued
