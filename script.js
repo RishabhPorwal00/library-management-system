2dc9325bde7eb4d1c1e8902afde20e76db3f3233
Rishabh Porwal <rishabhporwal06@gmail.com>
2026-09-10 23:52:52 +0530

    Change admin username to Rishabh in login check

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function login() {
   let username = document.getElementById("username").value.trim();
   let password = document.getElementById("password").value.trim();
 
-  if (username === "admin" && password === "1234") {
+  if (username === "Rishabh " && password === "1234") {
 
     document.getElementById("loginPage").style.display = "none";
     document.getElementById("app").style.display = "block";
